fe27fea8f89e57b8d46ab08910197e198f51a55e
Yan P Aung <yan.pye.aung@gmail.com>
2020-07-20 20:54:14 -0700

    Add static index.html file

--- Startup.cs
+++ Startup.cs
@@ -21,20 +21,8 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
