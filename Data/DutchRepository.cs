503531690813ba21e428214bf13e22e023241a78
Yan P Aung <yan.pye.aung@gmail.com>
2020-08-02 17:12:47 -0700

    Add some logging and configure logging levels: Module 7 complete!

--- Data/DutchRepository.cs
+++ Data/DutchRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,17 +10,29 @@ namespace DutchTreat.Data
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext _ctx;
+        private readonly ILogger<DutchRepository> _logger;
 
-        public DutchRepository(DutchContext ctx)
+        public DutchRepository(DutchContext ctx, ILogger<DutchRepository> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return _ctx.Products
-                       .OrderBy(p => p.Title)
-                       .ToList();
+            try
+            {
+                _logger.LogInformation("GetAllProducts was called");
+
+                return _ctx.Products
+                           .OrderBy(p => p.Title)
+                           .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all products: {ex}");
+                return null;
+            }
         }
 
         public IEnumerable<Product> GetProductsByCategory(string category)
