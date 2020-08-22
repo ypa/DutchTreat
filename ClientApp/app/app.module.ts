b7c2293af5b2c294c986937b617821e9c03aabb4
Yan P Aung <yan.pye.aung@gmail.com>
2020-08-21 19:59:41 -0700

    Add angular components for shopping cart

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { ProductList } from './shop/productList.component';
+import { Cart } from './shop/cart.component';
+import { DataService } from './shared/dataService';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductList
+    ProductList,
+    Cart
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    DataService 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
